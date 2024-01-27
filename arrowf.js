ff3a60990f54943ef4d567e0eaf3f8874b25474c
Moure <sadiahossainmoure@gmail.com>
2024-01-27 10:56:04 +0600

    adding

---
+++ arrowf.js
@@ -0,0 +1,9 @@
+function moure() {
+    console.log('sadia hossain');
+}
+moure();
+
+const name = (a,b)=>{
+    console.log(a+b);
+}
+name(2,6);
--- react.js
+++
@@ -1,2 +0,0 @@
-const name = 'sadia hossain moure';
-console.log(name);
